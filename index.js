b987b4ae40d58c8fa0bf61f1cab9f66c58b128d3
Bohdan Lysov <BogUkraine@gmail.com>
2020-09-05 13:34:13 +0300

    main structure

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
-const express = require('express');
+const express  = require('express');
+const mongoose = require('mongoose');
+const router   = require('./lib/router');
+const cors     = require('cors');
+const config   = require('./config');
 
 const app = express();
-const PORT = process.env.PORT || 8000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
+app.use('/api', router);
 
 const start = async () => {
     try {
-        app.listen(PORT, () => {
-            console.log(`Server has started on port ${PORT}`);
+        await mongoose.connect(config.mongoURI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true,
+            useFindAndModify: false,
+        });
+
+        app.listen(config.port, () => {
+            console.log(`Server has started on port ${config.port}`);
         })
     } catch (err) {
         console.log('Something went wrong: ', err);
