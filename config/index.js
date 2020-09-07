1981c7559b961d5773ff1947351415c81b859bb9
Bohdan Lysov <BogUkraine@gmail.com>
2020-09-07 19:28:50 +0300

    added google auth

--- config/index.js
+++ config/index.js
@@ -2,10 +2,15 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 module.exports = {
-    port            : process.env.PORT,
-    mongoURI        : process.env.MONGO_URI,
-    newsApiKey      : process.env.NEWS_API_KEY,
-    cronTimezone    : process.env.CRON_TIMEOUT_TIMEZONE,
-    cronTimeout     : process.env.CRON_TIMEOUT,
-    jwtSecret       : process.env.JWT_SECRET
+    port               : process.env.PORT,
+    mongoURI           : process.env.MONGO_URI,
+    newsApiKey         : process.env.NEWS_API_KEY,
+    cronTimezone       : process.env.CRON_TIMEOUT_TIMEZONE,
+    cronTimeout        : process.env.CRON_TIMEOUT,
+    jwtSecret          : process.env.JWT_SECRET,
+    googleClientId     : process.env.GOOGLE_CLIENT_ID,
+    googleClientSecret : process.env.GOOGLE_CLIENT_SECRET,
+    googleRedirectUrl  : process.env.GOOGLE_REDIRECT_URL,
+    googleAccessType   : process.env.GOOGLE_ACCESS_TYPE,
+    googleScope        : process.env.GOOGLE_SCOPE,
 }
